379b3c8c499ca8dfcc836b4ca91a43fc61713a5a
MarcosEmanuelBlanco <110629582+MarcosEmanuelBlanco@users.noreply.github.com>
2023-09-05 19:03:34 -0300

    Cambios 5/9/2023
    Se agregaron animaciones para el personaje principal, nuevas imágenes para las demás entidades, sonidos de salto y colisión con el suelo y un sistema de partículas a modo de generador de proyectiles.

--- My project/Assets/Scripts/Movimiento.cs
+++ My project/Assets/Scripts/Movimiento.cs
@@ -14,11 +14,21 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Variable para referenciar otro componente del objeto
     private Rigidbody2D miRigidbody2D;
+    private Animator miAnimator;
+    private SpriteRenderer miSprite;
+    private BoxCollider2D miCollider2D;
 
+    private int saltarMask;
+    
+    //private bool ladoIzq = false;
     // Codigo ejecutado cuando el objeto se activa en el nivel
     private void OnEnable()
     {
         miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAnimator = GetComponent<Animator>();
+        miSprite = GetComponent<SpriteRenderer>();
+        miCollider2D = GetComponent<BoxCollider2D>();
+        saltarMask = LayerMask.GetMask("Plataformas");
     }
 
     // Codigo ejecutado en cada frame del juego (Intervalo variable)
@@ -26,9 +36,18 @@ public class NewBehaviourScript : MonoBehaviour
     {
         moverHorizontal = Input.GetAxis("Horizontal");
         direccion = new Vector2(moverHorizontal, 0f);
+        int velocidadX = (int)miRigidbody2D.velocity.x;
+        miSprite.flipX = velocidadX < 0;
+        miAnimator.SetInteger("Velocidad", velocidadX);
+        miAnimator.SetBool("EnAire", !EnContactoConPlataforma());
     }
     private void FixedUpdate()
     {
         miRigidbody2D.AddForce(direccion * velocidad);
+    }
+
+    private bool EnContactoConPlataforma()
+    {
+        return miCollider2D.IsTouchingLayers(saltarMask);
     }
 }
